5c4fb5f4aebb546fbc0335ebfd5145a6897841dc
Kurt Kim <aurox0123@gmail.com>
2020-09-30 13:35:41 -0400

    Initial Commit

---
+++ 500px/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  500px
+//
+//  Created by Kurt Kim on 2020-09-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
