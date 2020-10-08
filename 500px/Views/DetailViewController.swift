4daf81bc7192afb9a84a2648db7e2cac6cf12951
Kurt Kim <aurox0123@gmail.com>
2020-10-08 01:41:18 -0400

    fixed scrollView and AutoLayout

--- 500px/Views/DetailViewController.swift
+++ 500px/Views/DetailViewController.swift
@@ -10,7 +10,6 @@ import Kingfisher
 
 class DetailViewController: UIViewController {
 
-    
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var hidingView: UIVisualEffectView!
@@ -20,7 +19,9 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupImageView()
         setupTextView()
+        setupTextViewConstraint()
         setupScrollView()
         setupTapGestures()
         let url = URL(string: photo?.images[photo.images.count - 1].url ?? "")
@@ -43,12 +44,36 @@ extension DetailViewController {
         self.textView.isEditable = false
         self.textView.isUserInteractionEnabled = false
     }
+    
+    func setupTextViewConstraint() {
+        self.textView.layer.masksToBounds = true
+        self.textView.translatesAutoresizingMaskIntoConstraints = false
+        let leadingConstraint = self.textView.leadingAnchor.constraint(equalTo: self.hidingView.leadingAnchor)
+        let trailingContraint = self.textView.trailingAnchor.constraint(equalTo: self.hidingView.trailingAnchor)
+        let centerConstraint = self.textView.centerYAnchor.constraint(equalTo: self.hidingView.centerYAnchor)
+        let heightConstraint = self.textView.heightAnchor.constraint(equalTo: self.hidingView.heightAnchor)
+        let widthConstraint = self.textView.widthAnchor.constraint(equalTo: self.hidingView.widthAnchor)
+        NSLayoutConstraint.activate([leadingConstraint,trailingContraint,centerConstraint,heightConstraint,widthConstraint])
+        self.hidingView.layoutIfNeeded()
+    }
+    
+    func setupImageView() {
+        self.imageView.isUserInteractionEnabled = true
+        self.imageView.contentMode = .scaleAspectFit
+        self.imageView.layer.masksToBounds = true
+        self.imageView.translatesAutoresizingMaskIntoConstraints = false
+        let leadingConstraint = self.imageView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor)
+        let trailingContraint = self.imageView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor)
+        let centerConstraint = self.imageView.centerYAnchor.constraint(equalTo: self.scrollView.centerYAnchor)
+        let heightConstraint = self.imageView.heightAnchor.constraint(equalTo: self.scrollView.heightAnchor)
+        let widthConstraint = self.imageView.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor)
+        NSLayoutConstraint.activate([leadingConstraint,trailingContraint,centerConstraint,heightConstraint,widthConstraint])
+        self.scrollView.layoutIfNeeded()
+    }
 }
 
 extension DetailViewController: UIScrollViewDelegate {
     func setupScrollView() {
-        self.imageView.isUserInteractionEnabled = true
-        self.imageView.contentMode = .scaleAspectFit
         self.scrollView.minimumZoomScale = 1.0
         self.scrollView.maximumZoomScale = 4.0
         self.scrollView.showsVerticalScrollIndicator = false
